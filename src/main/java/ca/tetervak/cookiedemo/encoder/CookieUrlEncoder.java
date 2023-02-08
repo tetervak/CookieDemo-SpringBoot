41c602a67d12fb18106f8536e1c9bba5a749c10d
Alexander Tetervak <tetervak@sheridancollege.ca>
2023-02-08 13:17:02 -0500

    simplified url encoder

--- src/main/java/ca/tetervak/cookiedemo/encoder/CookieUrlEncoder.java
+++ src/main/java/ca/tetervak/cookiedemo/encoder/CookieUrlEncoder.java
@@ -6,25 +6,18 @@ import org.springframework.stereotype.Component;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class CookieUrlEncoder implements CookieEncoder {
 
     @Override
     public String decode(String value){
-        try {
-            return URLDecoder.decode(value,java.nio.charset.StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UnsupportedEncodingException: " + e.getMessage());
-        }
+        return URLDecoder.decode(value, StandardCharsets.UTF_8);
     }
 
     @Override
     public String encode(String value){
-        try {
-            return URLEncoder.encode(value,java.nio.charset.StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UnsupportedEncodingException: " + e.getMessage());
-        }
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
     }
 }
