91a1245a3fc4c254e6756135ff9d011674d3d9e8
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-30 20:33:54 -0400

    converted to gradle and upgraded libs

--- src/main/java/ca/tetervak/cookiedemo/controller/CookieDemoController.java
+++ src/main/java/ca/tetervak/cookiedemo/controller/CookieDemoController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class CookieDemoController {
